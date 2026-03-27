2e35b40befebc6128394826d17c4d0c1e4b7eeb2
User <User@DESKTOP-6KNLMQJ>
2026-03-27 16:40:59 +0500

    Directions DONE

---
+++ Blazor/Academy/Migrations/20260327105818_mssql.local_migration_176.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Academy.Migrations
+{
+    /// <inheritdoc />
+    public partial class mssqllocal_migration_176 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Direction",
+                columns: table => new
+                {
+                    directin_id = table.Column<byte>(type: "tinyint", nullable: false),
+                    direction_name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Direction", x => x.directin_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Direction");
+        }
+    }
+}
