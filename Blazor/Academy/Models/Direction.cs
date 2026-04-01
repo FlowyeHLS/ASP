0fa05b87e6e1f91eca95a669781414c7cfd537eb
User <User@DESKTOP-6KNLMQJ>
2026-04-01 11:03:17 +0500

    UniqueDirectionName Attribute by Nika is DONE

--- Blazor/Academy/Models/Direction.cs
+++ Blazor/Academy/Models/Direction.cs
@@ -1,13 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Academy.Models.ValidationAtributes;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace Academy.Models
 {
+    [Index(nameof(direction_name), IsUnique =true)]
     public class Direction
     {
         [Key]
         public byte direction_id { get; set; }
+        [Required]
+        [UniqDirectionName(ErrorMessage = "List direcion name cyshestvyet")]
         public string direction_name { get; set; }
-
+        //public override bool Equals(object? other)
+        //{
+        //    return this.direction_name.Equals((other as Direction).direction_name);
+        //}
+        //public override int GetHashCode()
+        //{
+        //    return HashCode.Combine(direction_name);
+        //}
 
 
     }
---
+++ Blazor/Academy/Models/ValidationAtributes/UniqDirectionNameAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using Academy.Data;
+
+namespace Academy.Models.ValidationAtributes
+{
+    public class UniqDirectionNameAttribute:ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null || string.IsNullOrWhiteSpace(value.ToString()))
+                return ValidationResult.Success;
+            string directionName = value.ToString();
+            IDbContextFactory<AcademyContext> dbContextFactory = validationContext.GetService<IDbContextFactory<AcademyContext>>();
+            if (dbContextFactory == null)
+                return new ValidationResult("Oshibka first");
+            using (var context = dbContextFactory.CreateDbContext())
+            {
+                bool exists = context.Directions.Any(d => d.direction_name.ToLower() == directionName.ToLower());
+                if (exists) return new ValidationResult(ErrorMessage ?? $"Directio '{directionName}' already exists");
+                return ValidationResult.Success;
+            }
+
+        }
+    }
+}
