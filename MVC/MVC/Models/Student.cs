1548cb87acd5d040dd0f7792592f45cbfe1784e3
User <User@DESKTOP-6KNLMQJ>
2026-05-25 17:25:29 +0500

    MVC Complited

--- MVC/MVC/Models/Student.cs
+++ MVC/MVC/Models/Student.cs
@@ -1,9 +1,17 @@
-﻿namespace MVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Models
 {
     public class Student
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "BezFamililii nikak........")]
+        [StringLength(50, ErrorMessage = "Abrakadabra") ]
+        [Display(Name = "Фамилия")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "BezImeni nikak........")]
+        [StringLength(50, ErrorMessage = "Abrakadabra")]
+        [Display(Name = "Имя")]
         public string FirstName { get; set; }
         public DateTime EnrollmentDate { get; set; }
 
