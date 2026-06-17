30969413afcfcd7c23136b4fc96c7ad63ddf31b6
User <User@DESKTOP-6KNLMQJ>
2026-06-17 17:33:52 +0500

    AlliPs

---
+++ API/API/Program.cs
@@ -0,0 +1,60 @@
+using API;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDB>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+app.MapGet("/todoitems", async (TodoDB db) => await db.Tasklist.ToListAsync());
+app.MapGet("/todoitems/complete", async (TodoDB db) => await db.Tasklist.Where(t => t.DONE).ToListAsync());
+app.MapGet
+    (
+    "/todoitems/{id}",
+    async (int id, TodoDB db) => await db.Tasklist.FindAsync(id) is TODO todo ? Results.Ok(todo) : Results.NotFound()
+    );
+app.MapPost
+    (
+        "/todoitems",
+        async (TODO todo, TodoDB db) =>
+        {
+            db.Tasklist.Add(todo);
+            await db.SaveChangesAsync();
+            return Results.Created($"/todoitems/{todo.ID}", todo);
+        }
+    );
+
+app.MapPut
+    (
+        "todoitems/{id}",
+        async(int id, TODO inputTask, TodoDB db) =>
+        {
+            TODO todo = await db.Tasklist.FindAsync(id);
+            if (todo is null) return Results.NotFound();
+            todo.Name = inputTask.Name;
+            todo.DONE = inputTask.DONE;
+
+            await db.SaveChangesAsync();
+            return Results.NoContent();
+        }
+
+    );
+
+app.MapDelete
+    (
+        "/todoitems/{id}",
+        async (int id, TodoDB db) =>
+        {
+            if(await db.Tasklist.FindAsync(id) is TODO todo)
+            {
+                db.Tasklist.Remove(todo);
+                await db.SaveChangesAsync();
+                return Results.NoContent();
+            }
+            return Results.NotFound();
+        }
+    );
+
+app.Run();
---
+++ API/API/TodoDB.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public class TodoDB:DbContext
+    {
+        public TodoDB(DbContextOptions<TodoDB> options) : base(options)
+        {
+        }
+        public DbSet<TODO> Tasklist => Set<TODO>();
+    }
+}
